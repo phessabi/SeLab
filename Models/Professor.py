cb0256cc57217a70ae09614c53f71d62855161cd
danial23mm <danial23mm@gmail.com>
2020-05-04 18:29:31 +0430

    Adding some features to professor in its init function

--- Models/Professor.py
+++ Models/Professor.py
@@ -2,5 +2,11 @@ from Models.Person import Person
 
 
 class Professor(Person):
-    def __init__(self, id_number: int, name: str):
+    def __init__(self, id_number: int, name: str, room_no: int, lab_no: int):
         super(Professor, self).__init__(id_number, name)
+        self.course_list = []
+        self.student_list = []
+        self.project_list = []
+        self.room_no = room_no
+        self.lab_no = lab_no
+
